0b360644573874d8e232310fbff49ca6a304ee29
maitrak <maitrak523@gmail.com>
2022-06-12 19:09:08 +0530

    ui bug fix

--- connection.php
+++ connection.php
@@ -8,5 +8,7 @@ $connection=mysqli_connect("kidseduworldindia.com","kidsedu_kidsedu","Dec@2019$"
     }
 
 ?>
+<head>
 <link rel="icon" type="image/x-icon" href="KEWI_LOGO.jpg">
 <title>KEWI</title>
+</head>
